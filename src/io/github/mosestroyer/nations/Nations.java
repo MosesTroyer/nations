d6c7a1cfb92845792bff14484139b1eb9d137562
David Lamar <dmlamar@mtu.edu>
2015-05-30 16:35:19 -0400

    generic comment added.

--- src/io/github/mosestroyer/nations/Nations.java
+++ src/io/github/mosestroyer/nations/Nations.java
@@ -3,7 +3,7 @@ package io.github.mosestroyer.nations;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Nations extends JavaPlugin {
-	
+	//generic comment
 	public void onEnable(){
 		
 		getLogger().info("Nations successfully enabled!");
