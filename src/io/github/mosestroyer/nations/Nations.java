3ba129c83b11d39288ffee0b2f23920e0a9ce4ca
David Lamar <dmlamar@mtu.edu>
2015-05-30 21:33:20 -0400

    Added some spells

--- src/io/github/mosestroyer/nations/Nations.java
+++ src/io/github/mosestroyer/nations/Nations.java
@@ -1,9 +1,28 @@
 package io.github.mosestroyer.nations;
 
+
+import io.github.mosestroyer.nations.spells.spellbooks.Heal;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Nations extends JavaPlugin {
-	//generic comment
+	
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if (cmd.getName().equalsIgnoreCase("heal")) { 
+			if(sender instanceof Player){
+				Heal hl = new Heal();
+				hl.doSpell((Player)sender, this);
+			}
+			return true;
+		}
+		return false; 
+	}
+
+	
 	public void onEnable(){
 		
 		getLogger().info("Nations successfully enabled!");
