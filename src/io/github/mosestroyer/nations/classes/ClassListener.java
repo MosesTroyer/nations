797053d90ec65e1978cc4fab003f71adf8a1e6d3
David Lamar <dmlamar@mtu.edu>
2015-06-28 15:10:30 -0400

    Working on classes

--- src/io/github/mosestroyer/nations/classes/ClassListener.java
+++ src/io/github/mosestroyer/nations/classes/ClassListener.java
@@ -29,9 +29,107 @@ public class ClassListener implements Listener{
 		Player p = e.getPlayer();
 		Connection c = DatabaseConnection.getConnection();
 	
+		//Acrobat Code
 		if(PlayerDAO.getPlayerClass(c, p.getUniqueId()).equals("acrobat") ){
-			if(a.){
+			if(a == Action.LEFT_CLICK_AIR){
 				//do acrobat stuff
+				p.sendMessage("acr");
+			}
+		}
+		
+		//Alchemist Code
+		if(PlayerDAO.getPlayerClass(c, p.getUniqueId()).equals("alchemist") ){
+			if(a == Action.LEFT_CLICK_AIR){
+				//do acrobat stuff
+				p.sendMessage("alc");
+			}
+		}
+		
+		//Archer Code
+		if(PlayerDAO.getPlayerClass(c, p.getUniqueId()).equals("archer") ){
+			if(a == Action.LEFT_CLICK_AIR){
+				//do acrobat stuff
+				p.sendMessage("arc");
+			}
+		}
+		
+		//Blacksmith code
+		if(PlayerDAO.getPlayerClass(c, p.getUniqueId()).equals("blacksmith") ){
+			if(a == Action.LEFT_CLICK_AIR){
+				//do acrobat stuff
+				p.sendMessage("bla");
+			}
+		}
+		
+		//King code
+		if(PlayerDAO.getPlayerClass(c, p.getUniqueId()).equals("king") ){
+			if(a == Action.LEFT_CLICK_AIR){
+				//do acrobat stuff
+				p.sendMessage("king");
+			}
+		}
+		
+		//Kinght Code
+		if(PlayerDAO.getPlayerClass(c, p.getUniqueId()).equals("knight") ){
+			if(a == Action.LEFT_CLICK_AIR){
+				//do acrobat stuff
+				p.sendMessage("knight");
+			}
+		}
+		
+		//Lumberjack Code
+		if(PlayerDAO.getPlayerClass(c, p.getUniqueId()).equals("lumberjack") ){
+			if(a == Action.LEFT_CLICK_AIR){
+				//do acrobat stuff
+				p.sendMessage("lumb");
+			}
+		}
+		
+		//Mage Code
+		if(PlayerDAO.getPlayerClass(c, p.getUniqueId()).equals("mage") ){
+			if(a == Action.LEFT_CLICK_AIR){
+				//do acrobat stuff
+				p.sendMessage("mag");
+			}
+		}
+		
+		//Miner Code
+		if(PlayerDAO.getPlayerClass(c, p.getUniqueId()).equals("miner") ){
+			if(a == Action.LEFT_CLICK_AIR){
+				//do acrobat stuff
+				p.sendMessage("min");
+			}
+		}
+		
+		//Peasant
+		if(PlayerDAO.getPlayerClass(c, p.getUniqueId()).equals("peasant") ){
+			if(a == Action.LEFT_CLICK_AIR){
+				//do acrobat stuff
+				p.sendMessage("Pea");
+			}
+		}
+		
+		//Pyromaniac Code
+		if(PlayerDAO.getPlayerClass(c, p.getUniqueId()).equals("pyromaniac") ){
+			if(a == Action.LEFT_CLICK_AIR){
+				//do acrobat stuff
+				p.sendMessage("pyr");
+			}
+		}
+		
+		//Swimmer Code
+		if(PlayerDAO.getPlayerClass(c, p.getUniqueId()).equals("swimmer") ){
+			if(a == Action.LEFT_CLICK_AIR){
+				//do acrobat stuff
+				p.sendMessage("swi");
+			}
+		}
+		
+		//Warrior Code
+		if(PlayerDAO.getPlayerClass(c, p.getUniqueId()).equals("warrior") ){
+			if(a == Action.LEFT_CLICK_AIR){
+				//do acrobat stuff
+				p.sendMessage("war");
 			}
 		}
 		
